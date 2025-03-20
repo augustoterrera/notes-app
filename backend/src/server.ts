dd30de8a424b3ed3f97a02b5d91553d08b804ed1
Augusto <augustoterrera@gmail.com>
2025-03-20 19:55:58 -0300

    Organice el archivo server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,37 +1,41 @@
 import express from 'express';
-import { connectDB } from './config/db.js';
 import dotenv from "dotenv";
-import { router as authRouter } from './routes/authRoutes.js';	
-import { router as noteRouter } from './routes/noteRoutes.js';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser'; 
 import cors from 'cors';
+import { connectDB } from './config/db.js';
+import { router as authRouter } from './routes/authRoutes.js';	
+import { router as noteRouter } from './routes/noteRoutes.js';
 
-// Agrega esto después de app.use(express.json())
+// Configuración de variables de entorno
+dotenv.config();
 
-
-
+// Instancias de express
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Configuración de CORS
 const corsOptions = {
-  origin: 'http://localhost:3000', // Solo permite solicitudes desde este origen
+  origin: 'http://localhost:4200/', // Solo permite solicitudes desde este origen
   credentials: true, // Permite enviar cookies u otros encabezados sensibles
 };
 
-dotenv.config();
+// Conexión a la base de datos
+connectDB();
 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors(corsOptions))
 
+// Rutas
+app.use('/api', authRouter);
+app.use('/api', noteRouter);
+
+
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Running http://localhost:${port}`);
 });
-connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
-app.use('/api', authRouter);
-app.use('/api', noteRouter);
