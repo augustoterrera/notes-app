96ccc533f1e020cd889c4a0b3369ef296807c197
Augusto <augustoterrera@gmail.com>
2025-03-20 21:26:09 -0300

    Rutas publicas y privadas de prueba con localStorage

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { NotesListComponent } from './features/notes/notes-list/notes-list.component';
+import { AuthGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'mi-memoria', component: NotesListComponent, canActivate: [AuthGuard]}
+];
